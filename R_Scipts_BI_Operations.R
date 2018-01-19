c1bced06a5ca47c3f953b354cdcbf97da04724c4
Vijay Vaidyanathan <vvijay26@gmail.com>
2018-01-19 11:22:09 -0800

    BI Operations using R commands
    "Superstore Sales Data" folder contains the files on which these operations are performed.

---
+++ R_Scipts_BI_Operations.R
@@ -0,0 +1,31 @@
+#Module 6
+
+setwd("C:/pgdds/Course 1/Module 6/Superstore Sales Data")
+
+getwd()
+
+market_fact <- read.csv("market_fact.csv", stringsAsFactors = FALSE)
+cust_dimen <- read.csv("cust_dimen.csv", stringsAsFactors = FALSE)
+orders_dimen <- read.csv("orders_dimen.csv", stringsAsFactors = FALSE)
+prod_dimen <- read.csv("prod_dimen.csv", stringsAsFactors = FALSE)
+shipping_dimen <- read.csv("shipping_dimen.csv", stringsAsFactors = FALSE)
+
+?merge
+
+merge1 <- merge(market_fact,cust_dimen,by="Cust_id")
+merge2 <- merge(merge1,orders_dimen,by="Ord_id")
+merge3 <- merge(merge2,prod_dimen,by="Prod_id")
+merge4 <- merge(merge3,shipping_dimen,by="Ship_id")
+
+
+#slice operation
+subset(merge4, merge4$Customer_Name = "JOHN CASTELL")
+
+#dice operation
+dice1 <- subset(merge4, merge4$Region == "ATLANTIC" & merge4$Order_Priority == "MEDIUM")subset(merge4, merge4$Customer_Name = "JOHN CASTELL")
+
+#rollup operation
+rollup1 <- aggregate(merge4$Sales, by=list(merge4$Province), FUN=sum)
+
+rollup2 <- aggregate(merge4$Sales, by=list(merge4$Ship_Mode), FUN=sum)
+rollup3 <- aggregate(merge4$Profit, by=list(merge4$Product_Sub_Category), FUN=sum)
